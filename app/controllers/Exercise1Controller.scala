9262d8107b1e5d8c9d29cc2eae4cd714f91f7cb4
Dave Gurnell <dave@underscore.io>
2019-05-31 07:45:52 +0100

    Exercise 1 solutions

--- app/controllers/Exercise1Controller.scala
+++ app/controllers/Exercise1Controller.scala
@@ -1,44 +1,47 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class Exercise1Controller @Inject() (
   val controllerComponents: ControllerComponents
-) extends BaseController {
+)(implicit ec: ExecutionContext) extends BaseController {
   /** A plain action. Nothing to do here. */
   def plain =
     Action {
-      Ok("TODO: Complete")
+      Ok("Yay")
     }
 
   /** Read the headers from the request and return them to the user. */
   def headers =
-    Action {
-      Ok("TODO: Complete")
+    Action { request =>
+      Ok(request.headers.toMap.toString)
     }
 
   /** Turn this into an asynchronous action */
   def async =
-    Action {
-      Ok("TODO: Complete")
+    Action.async {
+      Future.successful(Ok("Yay"))
     }
 
   /** Return JSON to the user */
   def json =
     Action {
-      Ok("TODO: Complete")
+      Ok(Json.obj("foo" -> "bar"))
     }
 
   /** Read JSON from the request body and echo it back to the user */
   def jsonEcho =
-    Action {
-      Ok("TODO: Complete")
+    Action(parse.json) { request =>
+      Ok(request.body)
     }
 
   /** Read JSON from the request body and return it to the user asynchronously */
   def jsonAsync =
-    Action {
-      Ok("TODO: Complete")
+    Action.async(parse.json) { request =>
+      Future.successful(Ok(request.body))
     }
 }
